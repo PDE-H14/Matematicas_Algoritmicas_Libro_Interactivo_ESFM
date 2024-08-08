2a95d980bfeedd234396764996d803088aed1378
Carlos <cdzamora02@outlook.es>
2024-08-08 16:03:52 -0600

    Add applet about reflexión respecto al eje y

--- source/javascripts/applets/002_transformaciones_lineales/reflection_y.js
+++ source/javascripts/applets/002_transformaciones_lineales/reflection_y.js
@@ -1,5 +1,5 @@
 var edge = 7; 
-var board = JXG.JSXGraph.initBoard('reflection_x_box', {
+var board = JXG.JSXGraph.initBoard('reflection_y_box', {
         boundingbox: [-edge, edge, edge, -edge],
         showCopyright:false,
         axis:true,
@@ -14,37 +14,37 @@ var board = JXG.JSXGraph.initBoard('reflection_x_box', {
     });
 
 
-var checkbox_x = board.create('checkbox', [1, 5.5, 'Reflejar respecto al eje $x$'], {});
+var checkbox_y = board.create('checkbox', [1, 5.5, 'Reflejar respecto al eje $y$'], {});
 
-function reflect_x(x, y){
-    if (checkbox_x.Value()){
-        return [x, -y];
+function reflect_y(x, y){
+    if (checkbox_y.Value()){
+        return [-x, y];
     }else{
         return [x, y];
     }
 }
 
 var head = board.create('circle',
-    [function(){return reflect_x(1, 2.6)},
+    [function(){return reflect_y(1, 2.6)},
     0.4],
     {fillColor: colorStr, name:''});
 
 var leg1 = board.create('line',
-    [function(){return reflect_x(0,0)},
-    function(){return reflect_x(1, 1)}],
+    [function(){return reflect_y(0,0)},
+    function(){return reflect_y(1, 1)}],
     {straightFirst:false, straightLast:false, strokeWidth:2});
 
 var leg2 = board.create('line',
-    [function(){return reflect_x(2, 0)},
-    function(){return reflect_x(1, 1)}],
+    [function(){return reflect_y(2, 0)},
+    function(){return reflect_y(1, 1)}],
     {straightFirst:false, straightLast:false, strokeWidth:2});
 
 var body = board.create('line',
-    [function(){return reflect_x(1,1)},
-    function(){return reflect_x(1,2)}],
+    [function(){return reflect_y(1,1)},
+    function(){return reflect_y(1,2)}],
     {straightFirst:false, straightLast:false, strokeWidth:2});
 
 var arms = board.create('line',
-    [function(){return reflect_x(0,2)},
-    function(){return reflect_x(2,2)}],
+    [function(){return reflect_y(0,2)},
+    function(){return reflect_y(2,2)}],
     {straightFirst:false, straightLast:false, strokeWidth:2});
