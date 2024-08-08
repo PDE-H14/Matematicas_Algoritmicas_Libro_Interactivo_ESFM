fa949f7327d6194396cf899810c6301d9bba87ad
Carlos <cdzamora02@outlook.es>
2024-08-08 09:52:56 -0600

    Refactor: add function to rotate in applet

--- source/javascripts/applets/002_transformaciones_lineales_2_01.js
+++ source/javascripts/applets/002_transformaciones_lineales_2_01.js
@@ -9,27 +9,33 @@ var edge = 7;
 
     var slider = board.create('slider',[[-5,-4],[5,-4],[0,0,360]], {name:'Ángulo'});
 
+    function rotate(x, y){
+        var radians = 2*Math.PI*(slider.Value()/360);
+        return [x*Math.cos(radians) - y*Math.sin(radians), x*Math.sin(radians) + y*Math.cos(radians)];
+    }
+
+
     var head = board.create('circle',
-                            [function(){ return [1*Math.cos(2*Math.PI*(slider.Value()/360)) - 2.6*Math.sin(2*Math.PI*(slider.Value()/360)), 1*Math.sin(2*Math.PI*(slider.Value()/360)) + 2.6*Math.cos(2*Math.PI*(slider.Value()/360))]},
+                            [function(){ return rotate(1, 2.6)},
                             0.4],
                             {fillColor: colorStr, name:''});
 
     var leg1 = board.create('line',
                             [[0,0],
-                            function(){ return [1*Math.cos(2*Math.PI*(slider.Value()/360)) - 1*Math.sin(2*Math.PI*(slider.Value()/360)), 1*Math.sin(2*Math.PI*(slider.Value()/360)) + 1*Math.cos(2*Math.PI*(slider.Value()/360))]}],
+                            function(){ return rotate(1, 1)}],
                             {straightFirst:false, straightLast:false, strokeWidth:2});
 
     var leg2 = board.create('line',
-                            [function(){ return [2*Math.cos(2*Math.PI*(slider.Value()/360)) - 0*Math.sin(2*Math.PI*(slider.Value()/360)), 2*Math.sin(2*Math.PI*(slider.Value()/360)) + 0*Math.cos(2*Math.PI*(slider.Value()/360))]},
-                            function(){ return [1*Math.cos(2*Math.PI*(slider.Value()/360)) - 1*Math.sin(2*Math.PI*(slider.Value()/360)), 1*Math.sin(2*Math.PI*(slider.Value()/360)) + 1*Math.cos(2*Math.PI*(slider.Value()/360))]}],
+                            [function(){ return rotate(2, 0)},
+                            function(){ return rotate(1, 1)}],
                             {straightFirst:false, straightLast:false, strokeWidth:2});
     
     var body = board.create('line',
-                            [function(){ return [1*Math.cos(2*Math.PI*(slider.Value()/360)) - 1*Math.sin(2*Math.PI*(slider.Value()/360)), 1*Math.sin(2*Math.PI*(slider.Value()/360)) + 1*Math.cos(2*Math.PI*(slider.Value()/360))]},
-                            function(){ return [1*Math.cos(2*Math.PI*(slider.Value()/360)) - 2*Math.sin(2*Math.PI*(slider.Value()/360)), 1*Math.sin(2*Math.PI*(slider.Value()/360)) + 2*Math.cos(2*Math.PI*(slider.Value()/360))]}],
+                            [function(){ return rotate(1,1)},
+                            function(){ return rotate(1,2)}],
                             {straightFirst:false, straightLast:false, strokeWidth:2});
 
     var arms = board.create('line',
-                            [function(){ return [0*Math.cos(2*Math.PI*(slider.Value()/360)) - 2*Math.sin(2*Math.PI*(slider.Value()/360)), 0*Math.sin(2*Math.PI*(slider.Value()/360)) + 2*Math.cos(2*Math.PI*(slider.Value()/360))]},
-                            function(){ return [2*Math.cos(2*Math.PI*(slider.Value()/360)) - 2*Math.sin(2*Math.PI*(slider.Value()/360)), 2*Math.sin(2*Math.PI*(slider.Value()/360)) + 2*Math.cos(2*Math.PI*(slider.Value()/360))]}],
+                            [function(){ return rotate(0,2)},
+                            function(){ return rotate(2,2)}],
                             {straightFirst:false, straightLast:false, strokeWidth:2});
